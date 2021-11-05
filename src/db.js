71d85238a22825df5fb5474cf5ccce60b3f865d1
root <root@DESKTOP-V5R7D1D.localdomain>
2021-11-05 11:21:41 +0900

    connect db and install sequelize

--- src/db.js
+++ src/db.js
@@ -2,20 +2,23 @@ import app from "./server";
 
 const mysql = require("mysql");
 
+
+
 app.get("/test", function(req, res){
     var connection = mysql.createConnection({
         host: process.env.DATABASE_ENDPOINT,
         port: process.env.DATABASE_PORT,
         user: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
-        database: "database-1"
+        database: "carrot"
     });
 
-    connection.connect().then(console.log("Connected to DB!"));
+    connection.connect();
 
-    const getUserQuery = "SELECT * FROM UserInfo;";
+    const getUserQuery = "SELECT * FROM User;";
     const getUserResult = connection.query(getUserQuery, function(err, rows, fields){
         if(err){
+            console.log(err);
             res.send("회원정보 조회할 수 없음.")
         }else{
             res.send(rows)
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -4,9 +4,9 @@ import express from "express";
 
 const rootRouter = express.Router();
 
-rootRouter.get("/", home);
+//rootRouter.get("/", home);
 //rootRouter.route("/join").get(getJoin).post(postJoin);
 //rootRouter.route("/login").get(getLogin).post(postLogin);
-rootRouter.get("/search", search);
+//rootRouter.get("/search", search);
 
 export default rootRouter;
